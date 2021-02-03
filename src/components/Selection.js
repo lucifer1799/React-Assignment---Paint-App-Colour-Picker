26de0504c408c84666fc76d500f41bb77e162d8e
lucifer1799 <47712039+lucifer1799@users.noreply.github.com>
2021-02-03 20:54:02 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,27 @@
+import React, { Component } from "react";
 
+export default class Selection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: "",
+    };
+  }
+  updateColor=(value)=>{
+      this.setState({
+          background:value,
+      })
+
+  }
+  render() {
+    return (
+      <div
+        className="fix-box"
+        style={{ background: this.state.background }}
+        onClick={() => this.props.applyColo(updateColor)}
+      >
+        <h2>Selection</h2>
+      </div>
+    );
+  }
+}
